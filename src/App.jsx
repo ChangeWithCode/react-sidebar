8537c801d3bb81aed7160b3074182cab1f013052
Qasim <qasimriaz814@gmail.com>
2023-06-12 17:13:43 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Sidebar from "./assets/Components/Sidebar/Sidebar";
+import Main from "./assets/Components/Main/Mian";
+function App() {
+  return (
+    <>
+      <Sidebar></Sidebar>
+      <Main></Main>
+    </>
+  );
+}
+
+export default App;
